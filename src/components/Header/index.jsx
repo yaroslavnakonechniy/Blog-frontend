eeabf73bcc8de5180f5d20ed5d655c8feca00b73
yaroslavnakonechniy <yaroslav99.n@gmail.com>
2026-07-30 14:56:33 +0300

    created logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,13 +4,18 @@ import { Link } from 'react-router-dom';
 
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
-import { useSelector } from 'react-redux';
-import { selectIsAuth } from '../../redux/slices/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectIsAuth } from '../../redux/slices/auth';
 
 export const Header = () => {
+  const dispatch = useDispatch();
   const isAuth = useSelector(selectIsAuth);
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if(window.confirm('Ви ХОЧЕТЕ ВИЙТЕ?')) {
+      dispatch(logout());
+    }
+  };
 
   return (
     <div className={styles.root}>
