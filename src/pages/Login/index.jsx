5c5a20de361d12558a20ecf9601ec1244840512c
yaroslavnakonechniy <yaroslav99.n@gmail.com>
2026-07-30 15:07:25 +0300

    created authorization token

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -28,8 +28,16 @@ export const Login = () => {
   });
 
   const onSubmit = (values) => {
-    dispatch(fetchAuth(values));
-  }
+    const data = await dispatch(fetchAuth(values));
+
+    if(data.payload) {
+      return alert("Не вдалося авторизуватися!");
+    }
+
+    if('token' in data.payload) {
+       window.localStorage.setItem('token', data.payload.token);
+    }
+  };
 
   if(isAuth) {
     return <Navigate to="/" />
