52d5e77110ebe2ec793cc539f35cc84508ff04b5
yaroslavnakonechniy <yaroslav99.n@gmail.com>
2026-07-30 14:48:16 +0300

    created avtorization

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,33 +1,35 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "../axios";
+import axios from "../../axios";
 
-export const fetchUserData = createAsyncThunk('auth/fetchUserData', async (params) => {
+export const fetchAuth = createAsyncThunk('auth/fetchAuth', async (params) => {
     const { data } = await axios.post('/auth/login', params);
     return data;
 });
 
 const initialState = { 
     data: null,
-     status: 'loading',
+    status: 'loading',
 };
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     extraReducers: {
-        [fetchUserData.pending]: (state) => {
+        [fetchAuth.pending]: (state) => {
             state.status = 'loading'
             state.data = null;
         },
-        [fetchUserData.fulfilled]: (state, action) => {
+        [fetchAuth.fulfilled]: (state, action) => {
             state.status = 'loaded'
             state.data = action.payload;
         },
-        [fetchUserData.rejected]: (state) => {
+        [fetchAuth.rejected]: (state) => {
             state.status = 'error'
             state.data = null;
         }
     }
 });
+
+export const selectIsAuth = (state) => Boolean(state.auth.data);
 
 export const authReducer = authSlice.reducer;
