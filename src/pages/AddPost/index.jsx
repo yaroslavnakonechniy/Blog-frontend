3726a7f47fb2d2fe9d7931da23f0cdd424a25143
yaroslavnakonechniy <yaroslav99.n@gmail.com>
2026-08-01 12:10:30 +0300

    created acces control for unautorazed users

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -3,11 +3,17 @@ import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import SimpleMDE from 'react-simplemde-editor';
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
+import { selectIsAuth } from '../../redux/slices/auth';
 
 export const AddPost = () => {
+
+  const isAuth = useSelector(selectIsAuth);
+
   const imageUrl = '';
   const [value, setValue] = React.useState('');
 
@@ -33,6 +39,10 @@ export const AddPost = () => {
     }),
     [],
   );
+
+  if(isAuth) {
+    return <Navigate to="/" />
+  }
 
   return (
     <Paper style={{ padding: 30 }}>
