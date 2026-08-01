9dd9c6d2e4e5c2e5439e4388f46ba45ebc2a7309
yaroslavnakonechniy <yaroslav99.n@gmail.com>
2026-08-01 12:39:17 +0300

    created loading and deleting files

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -9,17 +9,33 @@ import { useSelector } from "react-redux";
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 import { selectIsAuth } from '../../redux/slices/auth';
+import axios from '../../axios';
 
 export const AddPost = () => {
 
   const isAuth = useSelector(selectIsAuth);
-
-  const imageUrl = '';
+  const [isLoading, setIsLoading] = React.useState(false); 
   const [value, setValue] = React.useState('');
+  const [title, setTetile] = React.useState('');
+  const [tags, setTags] = React.useState('');
+  const [imageUrl, setImageUrl] = React.useState('');
+  const inputFileRef = React.useRef(null);
 
-  const handleChangeFile = () => {};
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      alert("Помилка при шавантаженні файла!");
+    }
+  };
 
-  const onClickRemoveImage = () => {};
+  const onClickRemoveImage = () => {
+    setImageUrl('');
+  };
 
   const onChange = React.useCallback((value) => {
     setValue(value);
@@ -40,23 +56,28 @@ export const AddPost = () => {
     [],
   );
 
-  if(isAuth) {
+  if(!window.localStorage.getItem("token") && !isAuth) {
     return <Navigate to="/" />
   }
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
         Загрузити превью
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input 
+        ref={inputFileRef} 
+        type="file" 
+        onChange={handleChangeFile}
+        hidden 
+        />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Видалити
-        </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Видалити
+          </Button>
+          <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        </>
       )}
       <br />
       <br />
@@ -64,9 +85,17 @@ export const AddPost = () => {
         classes={{ root: styles.title }}
         variant="standard"
         placeholder="Заголовок статьи..."
+        value={title}
+        onChange={(e) => setTetile(e.target.value)}
         fullWidth
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
+      <TextField 
+        classes={{ root: styles.tags }} 
+        variant="standard" 
+        placeholder="Тэги" 
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        fullWidth />
       <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
       <div className={styles.buttons}>
         <Button size="large" variant="contained">
